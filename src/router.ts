5e719f3114283589d6f48c61e22d7e3f110bfcfe
Igor Meireles <igor.meireles.dev@gmail.com>
2022-11-15 12:26:46 -0300

    first commit

---
+++ src/router.ts
@@ -0,0 +1,51 @@
+import path from 'node:path';
+import { Router } from 'express';
+import multer from 'multer';
+
+import { listCategories } from './app/models/useCases/categories/listCategories';
+import { createCategory } from './app/models/useCases/categories/createCategory';
+import { listProducts } from './app/models/useCases/Products/listProducts';
+import { createProduct } from './app/models/useCases/Products/createProduct';
+import { listProductsByCategory } from './app/models/useCases/categories/listProductsByCategory';
+import { listOrders } from './app/models/useCases/Orders/listOrders';
+import { createOrder } from './app/models/useCases/Orders/createOrder';
+import { changeOrderStatus } from './app/models/useCases/Orders/changeOrderStatus';
+import { cancelOrder } from './app/models/useCases/Orders/cancelOrder';
+
+export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    }
+  }),
+});
+
+// List categories
+router.get('/categories', listCategories);
+
+// Create category
+router.post('/categories', createCategory);
+
+// List products
+router.get('/products', listProducts);
+
+// Create product
+router.post('/products', upload.single('image'), createProduct);
+
+// List products by category
+router.get('/categories/:categoryId/products', listProductsByCategory);
+
+// List orders
+router.get('/orders', listOrders);
+
+// Create order
+router.post('/orders', createOrder);
+
+router.patch('/orders/:orderId', changeOrderStatus);
+
+router.delete('/orders/:orderId', cancelOrder);
